e1eecbbbd84c67f54e19e5277fea9425219ebb1a
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-08 23:07:42 +0200

    Install Tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
+
+        @vite('resources/css/app.css')
     </head>
-    <body>
-        <h1>Welcome to the one and only Marslanders!</h1>
+    <body class="bg-pink-100">
+        <div class="flex justify-center">
+            <h1 class="p-8 text-3xl text-pink-700">
+                Welcome to the one and only Marslanders!
+            </h1>
+        </div>
     </body>
 </html>
