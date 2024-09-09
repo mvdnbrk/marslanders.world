f953bbfd8e0f0bf12a597e6a4bd10b02b5f00cd5
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-09 13:05:56 +0200

    Add Marslander model and migration

---
+++ database/migrations/2024_09_09_105053_create_marslanders_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('marslanders', function (Blueprint $table) {
+            $table->id();
+            $table->string('inscription_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('marslanders');
+    }
+};
