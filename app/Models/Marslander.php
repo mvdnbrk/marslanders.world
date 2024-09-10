38c07532b95b9e8ff8207eb7b91b63f115162012
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-10 16:22:20 +0200

    Update Marslander.php

--- app/Models/Marslander.php
+++ app/Models/Marslander.php
@@ -3,10 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Marslander extends Model
 {
+    protected $keyType = 'string';
+
+    protected $primaryKey = 'inscription_id';
+
     protected $hidden = [
         'hash',
     ];
+
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => 'https://cdn.marslanders.world/images/inscription/'.$this->id.'_500_'.$this->hash.'.webp',
+        );
+    }
 }
