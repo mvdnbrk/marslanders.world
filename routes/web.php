bf4756f1d708c862cda607cc8e6f867971c2117b
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-10 11:52:27 +0200

    Add HomepageController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomepageController::class)->name('home');
