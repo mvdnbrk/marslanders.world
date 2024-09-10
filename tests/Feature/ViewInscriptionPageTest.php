6e45e3e8ff527ae8c8ab88a9a2adcaec37c9b21e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-11 01:56:35 +0200

    Update ViewInscriptionPageTest.php

--- tests/Feature/ViewInscriptionPageTest.php
+++ tests/Feature/ViewInscriptionPageTest.php
@@ -2,14 +2,21 @@
 
 namespace Tests\Feature;
 
-use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
+use App\Models\Marslander;
+use Illuminate\Http\ResponseTrait;
+use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewInscriptionPageTest extends TestCase
 {
+    use RefreshDatabase;
+
     #[Test]
     public function a_request_to_the_inscription_page_should_return_a_200_status_code(): void
     {
+        $marslander = Marslander::factory()->create();
+
         $this->get('/inscription/d69deae6e29207fbc1b46a7a878ad631f790aadc3b5d00da4b9960c371eeae62i0')
             ->assertOk()
             ->assertViewIs('marslander');
