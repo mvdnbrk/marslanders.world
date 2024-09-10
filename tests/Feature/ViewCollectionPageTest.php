e38b045c4fcd91e4e228a14fe066e0e532eadd58
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-11 01:45:59 +0200

    Add tests

---
+++ tests/Feature/ViewCollectionPageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ViewCollectionPageTest extends TestCase
+{
+    #[Test]
+    public function a_request_to_the_collection_page_should_return_a_200_status_code(): void
+    {
+        $this->get('/collection')
+            ->assertOk()
+            ->assertViewIs('collection');
+    }
+}
--- tests/Feature/ViewHomepageTest.php
+++ tests/Feature/ViewHomepageTest.php
@@ -10,6 +10,8 @@ class ViewHomepageTest extends TestCase
     #[Test]
     public function a_request_to_the_homepage_should_return_a_200_status_code(): void
     {
-        $this->get('/')->assertOk()->assertViewIs('welcome');
+        $this->get('/')
+            ->assertOk()
+            ->assertViewIs('welcome');
     }
 }
