53282ad932d7dbf5c0c0177cb8ca7f8ace6b5a6b
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-11 11:43:30 +0200

    Update footer.blade.php

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="mt-12 mb-2 py-8 border-t border-pink-200">
     <p class="text-center text-xs leading-5 text-pink-300">
         &copy; 2024 {{ config('app.name') }}. All rights reserved.
     </p>
