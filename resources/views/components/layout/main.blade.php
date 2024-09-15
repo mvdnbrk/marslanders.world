6ef2c45710d7470b49dd93a93823d9e9293fdb8d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-15 21:46:31 +0200

    Add dark mode background color

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -12,7 +12,7 @@
         <script src="https://cdn.usefathom.com/script.js" data-site="{{ config('services.fathom.site_id') }}" defer></script>
         @endif
     </head>
-    <body class="bg-orange-200">
+    <body class="bg-orange-200 dark:bg-slate-900">
         <x-nav-header/>
         <main>
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
