3953c58eb388544128860bee3e688c958789ca5b
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-19 10:15:32 +0200

    Update ViewInscriptionPageTest.php

--- tests/Feature/ViewInscriptionPageTest.php
+++ tests/Feature/ViewInscriptionPageTest.php
@@ -21,7 +21,7 @@ class ViewInscriptionPageTest extends TestCase
     #[Test]
     public function a_request_to_the_inscription_page_should_return_a_200_status_code(): void
     {
-        $marslander = Inscription::factory()->create();
+        $inscription = Inscription::factory()->create();
 
         $this->get('/inscription/d69deae6e29207fbc1b46a7a878ad631f790aadc3b5d00da4b9960c371eeae62i0')
             ->assertOk()
