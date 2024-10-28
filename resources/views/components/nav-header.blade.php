532b71e19b7c2aedad3090ea75665781f63730f6
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-28 15:51:53 +0100

    Add BUY NOW button

--- resources/views/components/nav-header.blade.php
+++ resources/views/components/nav-header.blade.php
@@ -1,18 +1,30 @@
-<nav class="pt-4 pb-2 mb-12 bg-rose-950 border-b">
+<nav class="py-4 mb-12 bg-rose-950 border-b">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between">
-            <a
-                class="text-orange-100 hover:text-orange-400"
-                href="{{ route('home') }}"
-            >
-                home
-            </a>
-            <a
-                class="text-orange-100 hover:text-orange-400"
-                href="{{ route('collection') }}"
-            >
-                collection
-            </a>
+        <div class="flex">
+        <span class="flex-grow">
+                <a
+                    class="text-orange-100 hover:text-orange-400"
+                    href="{{ route('home') }}"
+                >
+                    home
+                </a>
+                <a
+                    class="ml-4 text-orange-100 hover:text-orange-400"
+                    href="{{ route('collection') }}"
+                >
+                    collection
+                </a>
+            </span>
+            <span class="ml-4 flex-shrink-0">
+                <a
+                    type="button"
+                    href="https://doggy.market/nfts/marslanders"
+                    target="_blank"
+                    class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100"
+                >
+                    BUY NOW
+                </a>
+            </span>
         </div>
     </div>
 </nav>
