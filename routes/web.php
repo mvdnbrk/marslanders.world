85b304a3de65d1917ffdcef1daaf5e061bd8cd70
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-29 11:31:56 +0100

    Indentation

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,17 @@ use App\Http\Controllers\InscriptionController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomepageController::class)->name('home');
+Route::get('/', HomepageController::class)
+    ->name('home');
 
-Route::get('/collection', CollectionController::class)->name('collection');
+Route::get('/collection', CollectionController::class)
+    ->name('collection');
 
 Route::get(
     '/inscription/{inscription:inscription_id}',
     InscriptionController::class
-)->name('inscription');
+)
+    ->name('inscription');
 
-Route::post('/search', SearchController::class)->name('search');
+Route::post('/search', SearchController::class)
+    ->name('search');
