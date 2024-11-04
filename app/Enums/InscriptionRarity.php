2b02ec37196515dd73c5713e397b0d7dad96b920
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-04 14:52:53 +0100

    Filter collection by rarity

--- app/Enums/InscriptionRarity.php
+++ app/Enums/InscriptionRarity.php
@@ -2,12 +2,41 @@
 
 namespace App\Enums;
 
+use Illuminate\Support\Collection;
+
 enum InscriptionRarity
 {
     case VERYRARE;
     case RARE;
     case UNCOMMON;
     case COMMON;
+
+    public static function slugs(): array
+    {
+        return array_map(
+            'strtolower',
+            array_map(fn ($case) => $case->name, self::cases())
+        );
+    }
+
+    public static function fromSlug(string $slug): ?self
+    {
+        return Collection::make(self::cases())
+            ->first(
+                fn ($case) => strtolower($slug) === strtolower($case->name)
+            );
+    }
+
+    public function rankRange(): array
+    {
+        return match ($this) {
+            self::VERYRARE => [1, 200],
+            self::RARE => [201, 1000],
+            self::UNCOMMON => [1001, 2500],
+            self::COMMON => [2501, 10000],
+            default => [1, 10000],
+        };
+    }
 
     public function name(): string
     {
--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\InscriptionRarity;
 use App\Models\Inscription;
 use Illuminate\View\View;
 
 class CollectionController extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(?string $rarity = null): View
     {
-        $inscriptions = Inscription::paginate(24);
+        $inscriptions = Inscription::query()
+            ->when($rarity !== null, function ($query) use ($rarity) {
+                $query->whereBetween(
+                    'rank',
+                    InscriptionRarity::fromSlug($rarity)->rankRange(),
+                );
+            })
+            ->paginate(24);
 
         return view('collection', [
             'inscriptions' => $inscriptions,
+            'rarity' => InscriptionRarity::fromSlug($rarity),
         ]);
     }
 }
