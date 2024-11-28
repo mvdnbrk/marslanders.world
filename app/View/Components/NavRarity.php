169ffe6845a1d30cf0fdf783a057ab6d5f7ef872
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-28 16:05:58 +0100

    code style

--- app/View/Components/NavRarity.php
+++ app/View/Components/NavRarity.php
@@ -2,9 +2,9 @@
 
 namespace App\View\Components;
 
-use Illuminate\View\Component;
 use App\Enums\InscriptionRarity;
 use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
 
 class NavRarity extends Component
 {
