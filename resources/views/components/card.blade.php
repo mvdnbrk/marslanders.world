10117dd225948eb028858964ff7bb06a74e33c1d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-28 18:03:20 +0100

    Update card.blade.php

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,9 @@
-<div {{ $attributes->merge(['class' => 'divide-y divide-stone-200 dark:divide-slate-600 overflow-hidden rounded-lg bg-stone-50 dark:bg-slate-800 shadow']) }}>
-    <div class="px-4 py-5 sm:p-6">
+<div
+{{ $attributes->merge([
+    'class' => 'divide-y divide-stone-200 dark:divide-slate-600 overflow-hidden rounded-lg bg-stone-50 dark:bg-slate-800 shadow'
+])
+}}>
+    <div class="p-4">
         <a
             @class([
                 'relative',
@@ -10,13 +14,12 @@
             href="{{ route('inscription', ['inscription' => $inscription]) }}"
         >
             <img
-                class="@class([
-                    'w-60',
-                    'rounded-md',
+                @class([
+                    'rounded-t-md',
                     'hover:scale-105',
                     'ease-out',
                     'duration-300',
-                ])"
+                ])
                 src="{{ $inscription->image_url }}"
                 alt="{{ $inscription->name }}"
                 width="500"
@@ -34,7 +37,7 @@
             @endif
         </a>
     </div>
-    <div class="px-4 py-4 sm:px-6">
+    <div class="px-4 py-4">
         <a
             class="text-orange-700 dark:text-slate-400 hover:underline underline-offset-2"
             href="{{ route('inscription', ['inscription' => $inscription]) }}"
