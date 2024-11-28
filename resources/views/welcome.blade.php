9d9b8994fe9b8ec03c5d1eb1f6120b82efffa5c6
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-28 16:38:41 +0100

    Add description on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 <x-layout.main>
     <div class="flex justify-center flex-col">
-        <div class="flex mb-12 items-center justify-center bg-marslanders-blue p-4 rounded-md">
+        <div class="flex flex-col mb-12 items-center justify-center bg-marslanders-blue p-4 rounded-md">
             <img
                 class="w-64 object-cover"
                 src="{{ asset('images/marslanders_logo.png') }}"
             >
+            <p class="text-sm font-bold tracking-wider text-yellow-400 mb-4">
+                The First High-Quality Recursive Revealer on Doginals!
+            </p>
         </div>
 
         <x-search/>
