5e6e4859e61b3620ed34cb8b62ffefc368d866a1
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-11 20:51:01 +0100

    Add Doge price

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,8 +1,11 @@
 <footer class="bg-white dark:bg-stone-950 opacity-90 mt-12 mb-2 py-8 border-t border-amber-100 dark:border-stone-600 drop-shadow-md">
-    <div class="flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 gap-x-16">
         <p class="grow text-sm leading-5 text-amber-800 dark:text-stone-400">
             &copy; 2024 {{ config('app.name') }}. All rights reserved.
         </p>
-        <x-twitter-link class="shrink-0 pr-2"/>
+        <p class="text-sm leading-5 text-amber-800 dark:text-stone-400">
+            DOGE PRICE: {{ Number::currency(Cache::get('doge_price'), in: 'USD') }}
+        <p>
+        <x-twitter-link class="-mt-4 shrink-0 pr-2"/>
     </div>
 </footer>
