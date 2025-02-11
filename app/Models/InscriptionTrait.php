248d2fbbe658e1d3eea877bf4a83b8a535b7cf18
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-11 17:01:04 +0100

    Add methods to retrieve trait type and values

--- app/Models/InscriptionTrait.php
+++ app/Models/InscriptionTrait.php
@@ -4,10 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class InscriptionTrait extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public static function getTypes(): Collection
+    {
+        return static::select('type')
+            ->groupBy('type')
+            ->orderBy('type')
+            ->pluck('type');
+    }
+
+    public static function getTypesAndValues(): Collection
+    {
+        return static::groupBy('type', 'value')
+            ->orderBy('type')
+            ->get()
+            ->groupBy('type')
+            ->map(function ($values) {
+                return $values->pluck('value')->values();
+            });
+    }
+
+    public static function getValuesForType(string $type): Collection
+    {
+        return static::getTypesAndValues()->get($type, Collection::make());
+    }
 }
