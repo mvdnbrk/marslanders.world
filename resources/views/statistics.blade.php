7079f2253fd6fd0cd1c0626c7c2672e576bdc158
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-12 17:04:28 +0100

    change margin top

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -25,7 +25,7 @@
         </div>
     </div>
 
-    <div class="mt-8 flow-root">
+    <div class="mt-5 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                 <div class="overflow-hidden ring-1 shadow-sm ring-black/5 sm:rounded-lg">
