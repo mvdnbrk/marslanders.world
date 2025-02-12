9a84a91ace40d071b3193680a27ec561b563349d
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-12 16:25:32 +0100

    Add a statistics page

--- app/Models/InscriptionTrait.php
+++ app/Models/InscriptionTrait.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Collection;
 
 class InscriptionTrait extends Model
@@ -11,6 +12,11 @@ class InscriptionTrait extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    public function inscription(): BelongsTo
+    {
+        return $this->belongsTo(Inscription::class);
+    }
 
     public static function getTypes(): Collection
     {
