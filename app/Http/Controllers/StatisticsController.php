ff5687287a7de099b8fc3923f77493d9af6c68c5
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-13 15:15:47 +0100

    Add command to update trait rarity

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Inscription;
 use App\Models\InscriptionTrait;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Number;
 use Illuminate\View\View;
@@ -20,28 +19,11 @@ class StatisticsController extends Controller
 
         $burn_count = Inscription::where('burned', true)->count();
 
-        $statistics = $this->getStatistics();
+        $statistics = InscriptionTrait::getStatistics();
 
         return view(
             'statistics',
             compact('doge_price', 'burn_count', 'statistics')
         );
-    }
-
-    protected function getStatistics(): Collection
-    {
-        return InscriptionTrait::selectRaw('
-            inscription_traits.type,
-            inscription_traits.value,
-            COUNT(*) as total_count,
-            SUM(CASE WHEN inscriptions.burned = true THEN 1 ELSE 0 END) as burned_count
-        ')
-            ->leftJoin('inscriptions', 'inscription_traits.inscription_id', '=', 'inscriptions.id')
-            ->groupBy('inscription_traits.type', 'inscription_traits.value')
-            ->orderBy('inscription_traits.type')
-            ->orderBy('inscription_traits.value')
-            ->get()
-            ->groupBy('type')
-            ->flatten();
     }
 }
