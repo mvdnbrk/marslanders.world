7096c3ce78852fe99593eebe3dc022ab0a3d5598
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-13 17:15:53 +0100

    fix typo

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('inscriptions:retrieve-burned')
     ->dailyAt('6:00');
-Schedule::command('marlanders:update-rarity')
+
+Schedule::command('marslanders:update-rarity')
     ->dailyAt('6:15');
 
 Schedule::command('doge:cache-price')->hourly();
