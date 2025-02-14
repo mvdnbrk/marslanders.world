fbb09be2d34b93745d7218ae81ef7d48cd8223f8
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-14 16:19:42 +0100

    update year

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-white dark:bg-stone-950 opacity-90 mt-12 mb-2 py-8 border-t border-amber-100 dark:border-stone-600 drop-shadow-md">
     <div class="flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 gap-x-16">
         <p class="grow text-sm leading-5 text-amber-800 dark:text-stone-400">
-            &copy; 2024 {{ config('app.name') }}. All rights reserved.
+            &copy; 2025 {{ config('app.name') }}. All rights reserved.
         </p>
         @if(Cache::has('doge_price'))
         <p class="text-sm leading-5 text-amber-800 dark:text-stone-400">
