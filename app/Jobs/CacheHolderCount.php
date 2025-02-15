6a6454a511985ad5754885b11273e67fc5b252de
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-15 14:03:18 +0100

    Revert "update headers"
    This reverts commit 0d1f0d41deec96d2fef775421f76ffaeca3eb173.

--- app/Jobs/CacheHolderCount.php
+++ app/Jobs/CacheHolderCount.php
@@ -25,22 +25,9 @@ class CacheHolderCount implements ShouldQueue
 
     public function handle(): void
     {
-        $headers = [
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
-            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Language' => 'nl-NL,nl;q=0.5',
-            'Sec-CH-UA' => '"Not(A:Brand";v="99", "Brave";v="133", "Chromium";v="133"',
-            'Sec-CH-UA-Mobile' => '?0',
-            'Sec-CH-UA-Platform' => '"macOS"',
-            'Sec-Fetch-Dest' => 'document',
-            'Sec-Fetch-Mode' => 'navigate',
-            'Sec-Fetch-Site' => 'none',
-            'Sec-Fetch-User' => '?1',
-            'Sec-GPC' => '1',
-            'Upgrade-Insecure-Requests' => '1',
-        ];
-
-        $response = Http::withHeaders($headers)
+        $response = Http::withHeaders([
+            'User-Agent' => request()->header('User-Agent'),
+        ])
             ->acceptJson()
             ->get($this->apiUrl);
 
